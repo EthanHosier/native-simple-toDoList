910ff669d6fcc4806e7b2826d606d67aac1b5b53
Ethan Hosier <ethanjhosier@gmail.com>
2022-12-25 22:17:31 +0000

    first commit

---
+++ App.js
@@ -0,0 +1,103 @@
+import { StatusBar } from 'expo-status-bar';
+import { Keyboard, KeyboardAvoidingView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import Task from './components/Tasks';
+import { useState } from 'react';
+
+export default function App() {
+  const [task,setTask] = useState("");
+  const [taskItems, setTaskItems] = useState([])
+
+  const handleAddTask = () =>{
+    Keyboard.dismiss();
+    setTaskItems([...taskItems, task])
+    setTask("");
+  }
+
+  const completeTask = (index) =>{
+    let itemsCopy = [...taskItems];
+    itemsCopy.splice(index, 1);
+    setTaskItems(itemsCopy);
+  }
+
+  return (
+    <View style={styles.container}>
+       {/*Today's tasks */}
+       <View style={styles.tasksWrapper}>
+        <Text style={styles.sectionTitle}>Today's tasks</Text>
+        
+        <View style={styles.items}>
+          {/*Where the tasks will go */}
+          {taskItems.map((item,i) =>{
+            return(
+              <TouchableOpacity key={i} onPress={() => completeTask(i)}>
+                <Task text={item}/>
+              </TouchableOpacity>
+            ) 
+          })}
+        </View>
+       </View>
+
+      {/*Write a task */}
+
+      <KeyboardAvoidingView
+      behavior={Platform.OS === "ios"? "padding" : "height"}
+      style={styles.writeTaskWrapper}
+      >
+        <TextInput style={styles.input} placeholder="Write a task" value={task} onChangeText={text => setTask(text)}/>
+
+        <TouchableOpacity onPress={() => handleAddTask()}>
+          <View style={styles.addWrapper}>
+            <Text style={styles.addText}>+</Text>
+          </View>
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#E8EAED',
+   
+  },
+  tasksWrapper:{
+    paddingTop: 80,
+    paddingHorizontal: 20,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  items:{
+    marginTop: 30,
+  },
+
+  writeTaskWrapper: {
+    position: 'absolute',
+    bottom: 60,
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-around"
+  },
+  input:{
+    paddingVertical: 15,
+    paddingHorizontal: 15,
+    backgroundColor: "#fff",
+    borderRadius: "60",
+    borderColor: "#C0C0C0",
+    borderWidth: 1,
+    width: 250,
+  },
+  addWrapper:{
+    width: 60,
+    height: 60,
+    backgroundColor: "#fff",
+    borderRadius: 60,
+    justifyContent: "center",
+    alignItems: "center",
+    borderColor: "#C0C0C0",
+    borderWidth: 1,
+  },
+  
+});
